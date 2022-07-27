886bad3b0325574a12e46e586f5fdac7bd51c534
Timothy Joshua Chua <timothy.d.chua@gmail.com>
2022-07-27 22:17:56 +0900

    Added existing project

---
+++ github.com/tdchua/firstapp/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+  "fmt"
+  "sync"
+)
+
+var wg = sync.WaitGroup{}
+
+func main() {
+    ch := make(chan int, 50)
+    wg.Add(2)
+    go func(ch <-chan int) {
+        for i := range ch {
+            fmt.Println(i)
+        }
+        wg.Done()
+    }(ch)
+    go func(ch chan <- int) {
+        ch <- 42
+        ch <- 27
+        wg.Done()    
+    }(ch)
+    wg.Wait()
+}
